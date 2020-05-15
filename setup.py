c8dbb89865707b928823b370d864daea97cd1a63
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2020-05-15 11:30:25 +0900

    Update package version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
-	long_description = fh.read()
+    long_description = fh.read()
 
 setup(
     name="lambdata-johnnykoo",
-    version='0.1.2',
+    version='0.1.3',
     description="My first python package practice",
     long_description="Hello World, this is my very first python package!",
     long_description_content_type="text/markdown",
@@ -13,5 +13,5 @@ setup(
     author_email="johnnykoo84@gmail.com",
     url="https://github.com/johnnykoo84/lambdata-johnnykoo84",
     license="MIT",
-    packages=find_packages() # ['my_labmdata']
+    packages=find_packages()  # ['my_labmdata']
 )
